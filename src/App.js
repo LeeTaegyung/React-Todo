ed91400a282509c75c429a9670df98586ec8b8aa
taegyung <taegyung.e@gmail.com>
2024-03-27 18:37:47 +0900

    작업 save

--- src/App.js
+++ src/App.js
@@ -133,6 +133,118 @@ function App() {
                     </button>
                   </div>
                 </li>
+                <li className="todo_item">
+                  <div className="check_item">
+                    <input type="checkbox" id="todo01" />
+                    <label for="todo01">투두01</label>
+                  </div>
+                  <div className="todo_modify">
+                    <button type="button" className="delete_btn">
+                      삭제
+                    </button>
+                    <button type="button" className="edit_btn">
+                      수정
+                    </button>
+                  </div>
+                </li>
+                <li className="todo_item">
+                  <div className="check_item">
+                    <input type="checkbox" id="todo02" />
+                    <label for="todo02">투두02</label>
+                  </div>
+                  <div className="todo_modify">
+                    <button type="button" className="delete_btn">
+                      삭제
+                    </button>
+                    <button type="button" className="edit_btn">
+                      수정
+                    </button>
+                  </div>
+                </li>
+                <li className="todo_item">
+                  <div className="check_item">
+                    <input type="checkbox" id="todo03" />
+                    <label for="todo03">투두03</label>
+                  </div>
+                  <div className="todo_modify">
+                    <button type="button" className="delete_btn">
+                      삭제
+                    </button>
+                    <button type="button" className="edit_btn">
+                      수정
+                    </button>
+                  </div>
+                </li>
+                <li className="todo_item">
+                  <div className="check_item">
+                    <input type="checkbox" id="todo04" />
+                    <label for="todo04">투두04</label>
+                  </div>
+                  <div className="todo_modify">
+                    <button type="button" className="delete_btn">
+                      삭제
+                    </button>
+                    <button type="button" className="edit_btn">
+                      수정
+                    </button>
+                  </div>
+                </li>
+                <li className="todo_item">
+                  <div className="check_item">
+                    <input type="checkbox" id="todo05" />
+                    <label for="todo05">투두05</label>
+                  </div>
+                  <div className="todo_modify">
+                    <button type="button" className="delete_btn">
+                      삭제
+                    </button>
+                    <button type="button" className="edit_btn">
+                      수정
+                    </button>
+                  </div>
+                </li>
+                <li className="todo_item">
+                  <div className="check_item">
+                    <input type="checkbox" id="todo06" />
+                    <label for="todo06">투두06</label>
+                  </div>
+                  <div className="todo_modify">
+                    <button type="button" className="delete_btn">
+                      삭제
+                    </button>
+                    <button type="button" className="edit_btn">
+                      수정
+                    </button>
+                  </div>
+                </li>
+                <li className="todo_item">
+                  <div className="check_item">
+                    <input type="checkbox" id="todo07" />
+                    <label for="todo07">투두07</label>
+                  </div>
+                  <div className="todo_modify">
+                    <button type="button" className="delete_btn">
+                      삭제
+                    </button>
+                    <button type="button" className="edit_btn">
+                      수정
+                    </button>
+                  </div>
+                </li>
+                <li className="todo_item">
+                  <div className="check_item">
+                    <input type="checkbox" id="todo08" />
+                    <label for="todo08">투두08</label>
+                  </div>
+                  <div className="todo_modify">
+                    <button type="button" className="delete_btn">
+                      삭제
+                    </button>
+                    <button type="button" className="edit_btn">
+                      수정
+                    </button>
+                  </div>
+                </li>
               </ul>
             </div>
           </div>
